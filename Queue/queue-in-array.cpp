553adcf98d44774846460e5b6e9ca45edc5f437c
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-07-14 20:51:16 -0300

    started two way to implement a queue

--- Queue/queue-in-array.cpp
+++ Queue/queue-in-array.cpp
@@ -9,7 +9,7 @@ struct People{
 
 
 /*make a program to manage the bank queue
- Input: A list of commands wich ends with p  = stop ;
+ Input: A list of commands wich ends with p  = stop;
  + = person name and age |Person enter in queue
  - = person name and | person quit the queue
  A = the person was served in the queue
@@ -21,13 +21,46 @@ int main(){
     People queue[10];
     int index = -1;
     
+    char input;
+
     People people;
-    people.name = "Thales";
-    people.age = 24;
 
-    cout << people.name << endl;
-    cout << people.age;
+    while(cin >> input && input != 'p'){
+        if(input == '+'){
+            //insert
+            cin >> people.name >> people.age;
+        }
 
+        else if(input == '-'){
+            //remove|quit
+        }
+        else if(input == 'a'){
+            //served
+        }
+        else{
+            cout << "invalid input";
+        }
+
+    }   
+
+    // em switch case
+
+    switch(input){
+        case '+':
+        //input
+        cin >> name;
+        break;
+
+        case '-':
+        //remove
+        break;
+        
+        case 'a':
+        //served
+        break;
+
+        default: cout << "invalid input !";
+    }
 
 
     return 0;
