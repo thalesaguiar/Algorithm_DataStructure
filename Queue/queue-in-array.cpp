77da30fb5ef4b9aefc78104ddd2c2092186f074f
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-07-16 01:04:56 -0300

    some logic erros fxed and some code implemented

--- Queue/queue-in-array.cpp
+++ Queue/queue-in-array.cpp
@@ -2,11 +2,11 @@ using namespace std;
 
 #include <iostream>
 
-struct People{
+struct People
+{
     string name;
     int age;
 };
-
 
 /*make a program to manage the bank queue
  Input: A list of commands wich ends with p  = stop;
@@ -14,62 +14,86 @@ struct People{
  - = person name and | person quit the queue
  A = the person was served in the queue
  */
- 
 
-int main(){
+int main()
+{
 
     int queueSize = 10;
     People queue[queueSize];
-    int index = -1;
-    
+    int currentPosition = -1;
+
     char input;
 
     People people;
 
-    while(cin >> input && input != 'p'){
-        if(input == '+'){
-            //insert
+    while (cin >> input && input != 'p')
+    {
+        if (input == '+')
+        { // insert
             cin >> people.name >> people.age;
 
-            //if the queue has sapce
+            // if the queue has sapce
 
-            if(index + 1 >= queueSize ){
-                cout <<"The queue is full";
+            if (currentPosition + 1 >= queueSize)
+            {
+                cout << "the queue is full!";
             }
-            
+            else
+            {
+                queue[++currentPosition] = people; // insert people
+            }
         }
 
-        else if(input == '-'){
-            //remove|quit
+        else if (input == '-')
+        { // remove|quit
+            // search the people by the name
         }
-        else if(input == 'a'){
-            //served
+        else if (input == 'a')
+        { // served
+            if (currentPosition == -1)
+            {
+                cout << "the queue is empt" << endl;
+                continue;
+            }
+
+            People next = queue[0];
+            cout << "attending " << next.name << endl;
+
+            //all people take the next position in queue
+            for(int i = 0 ; i < currentPosition; i++){
+                queue[i] = queue[i+1];
+            }
+
+            //refresh the position of last people in queue
+            currentPosition = currentPosition - 1;
         }
-        else{
+
+        else
+        {
             cout << "invalid input";
         }
-
-    }   
+    }
 
     // em switch case
 
-    switch(input){
-        case '+':
-        //input
+    switch (input)
+    {
+    case '+':
+        // input
         cin >> name;
         break;
 
-        case '-':
-        //remove
-        break;
-        
-        case 'a':
-        //served
+    case '-':
+        // remove
         break;
 
-        default: cout << "invalid input !";
+    case 'a':
+        // served
+        break;
+
+    default:
+        cout << "invalid input !";
     }
-
 
     return 0;
 }
