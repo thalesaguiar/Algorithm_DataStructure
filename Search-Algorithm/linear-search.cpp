f9e90a7a5cea83c29de6802760c158f73b6b2251
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-07-21 00:16:53 -0300

    a linear algorithm implementation

--- Search-Algorithm/linear-search.cpp
+++ Search-Algorithm/linear-search.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){ 
+int indexOf(int arr[], int size, int value){
+    for (int i = 0; i < size; i++){
+        if (arr[i] == value){
+            return i;
+        }
+    }
+    return -1;
+}
 
+int main(){
 
-
-
-
+    int arr[] = {0, 12, 2, 3, 5, 7, 10, 1, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]); // sizeof retorna o tamanho em bites de um array.
+    int value = 1;
+    int index = indexOf(arr, size, value);
+    if (index > -1){
+        cout << "The index of the value " << value << " in array is " << index;
+    }
+    else{
+        cout << "the number" << value << "don't exist in array";
+    }
 
     return 0;
 }
