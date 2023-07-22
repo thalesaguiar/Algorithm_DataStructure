cf2a7191a089bfaf27dba3ba998557679936eb09
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-07-22 20:28:03 -0300

    some improvments in algorithm

--- Search-Algorithm/binary-search.cpp
+++ Search-Algorithm/binary-search.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int BinarySearch(int arr[],int size,int left,int right, int middle, int number){
-    for(int i = 0; i < size; i++){
-        middle = (left+right)/2;
-        if(arr[middle] == number){
-            cout << "O número " << number << " is in the set!";
-            return 1;
+int BinarySearch(int arr[],int size, int value){
+    int left = 0;
+    int right = size -1;
+
+    while(left <=right){
+        int middle = int((left+right)/2);//force the result always inter
+        if(arr[middle] == value){
+            return middle;
         }
-        else if(arr[middle] < number){
+        else if(arr[middle] < value){
             left = middle + 1;
         }
-        else if(arr[middle] > number){
-            right = middle - 1;
+        else{
+            right = middle -1;
         }
     }
-        cout << "O número " << number << " it is not in the set.";
     return -1;
 }
 
 int main(){
 
-    int size = 5, left = 0, right = 4, middle = 0, number;
-    int arr[size] = {1,2,3,4,5};
+    int arr[] = {1,2,3,4,5,6,7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int value;
 
     cout << "Enter a number to check if it belongs to the set of numbers."<< endl;
-    cin >> number;
+    cin >> value;
 
-    BinarySearch(arr,size,left,right,middle,number);
+    int index = BinarySearch(arr,size,value);
+    
+    if(index == -1){
+        cout << "The number " << value << " it is not at the set" << endl;
+    }
+    else{
+    cout << "The number " << value << " is at the position " << index << endl;
+    }
 
     return 0;
 }
