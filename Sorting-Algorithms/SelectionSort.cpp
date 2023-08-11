e909f0faa41bed36705a09cebb1b8b05323f3012
thalesaguiar <dev.thalesaguiar@gmail.com>
2023-08-11 15:52:33 -0300

    selection sort implementation

--- Sorting-Algorithms/SelectionSort.cpp
+++ Sorting-Algorithms/SelectionSort.cpp
@@ -3,14 +3,42 @@
 using namespace std;
 
 void selectionSort(int numbers[],int n){
-
-    for(int i = 0; i < n; i++ ){
-        int smallerindex = i;
-    
-        for(int j = i + 1; j < n; j++){
-            if(numbers[j] < numbers[smallerindex])
-            int a;
-        }
+  for(int i = 0; i < n - 1; i++){
+    int smallerIndex = i;
+    for(int j = i + 1 ; j < n; j++){
+      if(numbers[j] < numbers[smallerIndex]){
+				smallerIndex = j;
+			}    
     }
+		if( i != smallerIndex){
+			//swap
+			int aux = numbers[i];
+			numbers[i] = numbers[smallerIndex];
+			numbers[smallerIndex] = aux;
+		}
+  }
+}
 
+int main() {
+
+  int size;
+  cout << "type the amount of numbers to be sorted" << endl;
+  cin >> size;
+
+  int *array = new int[size];
+
+  cout << "type the numbers that will be sorted" << endl;
+
+	for (int i = 0; i < size; i++){
+		cin >> array[i];
+	}
+		
+	selectionSort(array, size);
+	
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << ' ';
+  }
+
+  delete[] array;
+  return 0;
 }
